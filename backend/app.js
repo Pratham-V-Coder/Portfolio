cc595107145c5f6bae77e373540e1ce42c9b0f45
Pratham <prathamverma8880@gmail.com>
2026-08-15 11:18:48 +0530

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-// import contactRouter from "./routers/contactRouter.js";
-// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: [process.env.CLIENT_URL, "http://localhost:5173"],
-//     credentials: true,
-//   }),
-// );
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     success: true,
-//     message: "Portfolio Backend Running 🚀",
-//   });
-// });
-
-// app.use("/api/v1/contact", contactRouter);
-
-// app.use(notFound);
-// app.use(errorHandler);
-
-// export default app;
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -57,8 +22,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
