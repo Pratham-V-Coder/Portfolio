c0ebc7148b5ecb6693d4a59e3f197f92512c1a5b
Pratham <prathamverma8880@gmail.com>
2026-08-15 09:49:53 +0530

    fix: call startServer function

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-import app from "./app.js";
-import connectDB from "./config/db.js";
-
-dotenv.config();
-
-// Connect MongoDB
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
